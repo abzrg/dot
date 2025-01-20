9eb850a706d975f690003a4fd8f9230ddfd8d715
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2025-01-20 23:39:10 +0330

    feat(scripts): tmux-cht ascii codes -> ascii colors
    For some reason 'less' only shows the color escape codes. To fix that, I
    simply added the -R/--RAW-CONTROL-CHARS flag to less

--- scripts/.local/bin/scripts/tmux-cht.sh
+++ scripts/.local/bin/scripts/tmux-cht.sh
@@ -12,9 +12,9 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less --RAW-CONTROL-CHARS"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less --RAW-CONTROL-CHARS"
 fi
 
 
