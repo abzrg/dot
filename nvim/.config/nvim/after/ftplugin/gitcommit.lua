9bec51acdd0052df534e2bca174973eb4ffc7336
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2025-01-24 14:23:25 +0330

    feat(nvim): gitcommit ftplugins setttings...
    - set proper 'formatoptions', 'textwidth', 'tabstop', 'shiftwidth'
      locally with vim.fn.defer of 1 ms.
      - this is done since nvim will modify this settings if we just set
        them even in the after/ftplugins folder. To fix this issue, we
        simply wait for 1ms after nvim is loaded and then set the settings
        so that they take effect. This also probably overwrite .editorconfig
        settings as well even if the 'vim.opt_local.editorconfig = false'
        does not take effect.
    - ignoring .editorconfig settings so that we don't overwrite these
      settings by mistake.
    - enable text wrapping since its annoying to scroll the text
      horizontally while writing commit messages
      - still not sure what 'linebreak' do since it does not break any line.
        Need further investigation on this option.
    - disable line numbering and 'signcolumn' since they are redundant
      information for a small file such as commit buffer.
    - set 'bufhidden' to 'delete'. this prevents us from mistakenly commit
      incomplete commit messages while the commit buffer is not displayed in
      the window, and we are save and quit all buffers while we are visiting
      another buffer.

--- nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,8 +1,43 @@
-vim.opt_local.formatoptions = "ljqnt"
+-- Disable editorconfig right away
+vim.b.editorconfig = false
 
--- https://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html
-vim.bo.textwidth = 72
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
+-- Enable the following settings after neovim has been loaded.
+-- This is done because otherwise neovim will overwrite them.
+-- Another way to achieve this is to set up autocommands but this is enough.
+vim.defer_fn(function()
+  vim.opt_local.colorcolumn = "50,72"
 
+  -- https://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html
+  vim.bo.textwidth = 72
+  vim.bo.tabstop = 2
+  vim.bo.shiftwidth = 2
+
+  vim.opt_local.formatoptions = {
+    c = false, -- Auto-wrap comments using 'textwidth'
+    l = true,  -- Auto-wrap long lines in insert mode
+    q = false, -- Allow formatting of comments with "gq"
+    t = true,  -- Auto-wrap text using 'textwidth'
+    n = true,  -- Recognize numbered lists.
+    j = true,  -- Remove a comment leader when joining
+  }
+end, 1)
+
+-- Enable spell checking
 vim.opt_local.spell = true
+vim.opt_local.spellcapcheck = ""
+
+-- Wrap text
+vim.opt_local.wrap = true
+-- Wrap long lines (visually) at 'breakat'
+vim.opt_local.linebreak = true
+
+-- We don't need line numbers
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+
+-- ...Nor we do signcolumns
+vim.opt_local.signcolumn = "no"
+
+-- If the git commit message buffer is not displayed then delete the buffer so that we
+-- don't save the incomplet git message by mistake.
+vim.opt_local.bufhidden = "delete"
