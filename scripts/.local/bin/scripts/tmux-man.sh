39b4a14dd152e19a5ae65cbac2e9c60e85acdd15
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2025-07-03 23:41:43 +0330

    script: update tmux man page selector

--- scripts/.local/bin/scripts/tmux-man.sh
+++ scripts/.local/bin/scripts/tmux-man.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -e
 
-man `fzf < ~/.manpages` 2>/dev/null
+export MANPATH="$HOME/opt/mpich/mpich-4.3.0/share/man:$MAPATH"
+
+/usr/bin/man "$(fzf --margin='20%'  --no-info --no-border --no-scrollbar \
+                   --separator='' --pointer='' --color='bw' \
+                   < ~/.manpages)" 2>/dev/null
